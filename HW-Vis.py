a29724d0f4e68c61bc2a128ef4c919aeab784322
savcoop <savcoop@umich.edu>
2018-12-03 16:55:07 -0500

    first function done

--- HW-Vis.py
+++ HW-Vis.py
@@ -5,14 +5,44 @@ import twitter_info # still need this in the same directory, filled out
 import matplotlib.pyplot as plt
 
 ## [PART 1]
-# Finish writing the function getDayDict which takes a database cursor and returns a 
-# dictionary that has the days of the weeks as the keys (using "Sun", "Mon", "Tue", 
-# "Wed", "Thu", "Fri", "Sat") and the number of tweets on the named day as the values
-#
-# cur - the database cursor
 def getDayDict(cur):
-    pass
+	cur.execute('SELECT time_posted FROM Tweets')
+	data = cur.fetchall()
+	dayDict = {}
+	sun = 0
+	mon = 0
+	tue = 0
+	wed = 0
+	thu = 0
+	fri = 0
+	sat = 0
+	for row in data:
+		day = row[0][0:3]
+		if day == "Sun":
+			sun += 1
+		elif day == "Mon":
+			mon += 1
+		elif day == "Tue":
+			tue += 1
+		elif day == "Wed":
+			wed += 1
+		elif day == "Thu":
+			thu += 1
+		elif day == "Fri":
+			fri += 1
+		elif day == "Sat":
+			sat += 1
 
+	dayDict["Sun"] = sun
+	dayDict["Mon"] = mon
+	dayDict["Tue"] = tue
+	dayDict["Wed"] = wed
+	dayDict["Thu"] = thu
+	dayDict["Fri"] = fri
+	dayDict["Sat"] = sat
+
+	retun(dayDict)
+  
 ## [Part 2]
 # Finish writing the function drawBarChart which takes the dictionary and draws a bar 
 # chart with the days of the week on the x axis and the number of tweets on the named day on 
